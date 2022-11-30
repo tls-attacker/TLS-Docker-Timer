d59a250074c57c5b2cf69a32815e2574e929787a
mmaehren <32199075+mmaehren@users.noreply.github.com>
2022-11-30 17:20:43 +0100

    Re-Implemented certificate fetcher to allow callbacks

--- src/main/java/de/rub/nds/timingdockerevaluator/task/subtask/BleichenbacherSubtask.java
+++ src/main/java/de/rub/nds/timingdockerevaluator/task/subtask/BleichenbacherSubtask.java
@@ -10,26 +10,39 @@ import de.rub.nds.tlsattacker.core.constants.AlgorithmResolver;
 import de.rub.nds.tlsattacker.core.constants.CipherSuite;
 import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.core.constants.KeyExchangeAlgorithm;
+import de.rub.nds.tlsattacker.core.exceptions.WorkflowExecutionException;
+import de.rub.nds.tlsattacker.core.protocol.message.CertificateMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.HelloVerifyRequestMessage;
 import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.util.CertificateFetcher;
+import static de.rub.nds.tlsattacker.core.util.CertificateFetcher.fetchServerCertificate;
 import de.rub.nds.tlsattacker.core.workflow.DefaultWorkflowExecutor;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowExecutor;
+import de.rub.nds.tlsattacker.core.workflow.WorkflowExecutorFactory;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTraceUtil;
+import de.rub.nds.tlsattacker.core.workflow.action.ReceiveAction;
 import de.rub.nds.tlsattacker.core.workflow.action.ReceiveTillAction;
+import de.rub.nds.tlsattacker.core.workflow.action.SendAction;
+import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory;
 import de.rub.nds.tlsscanner.serverscanner.probe.bleichenbacher.constans.BleichenbacherScanType;
 import de.rub.nds.tlsscanner.serverscanner.probe.bleichenbacher.constans.BleichenbacherWorkflowType;
 import de.rub.nds.tlsscanner.serverscanner.probe.bleichenbacher.trace.BleichenbacherWorkflowGenerator;
 import de.rub.nds.tlsscanner.serverscanner.probe.bleichenbacher.vector.Pkcs1Vector;
 import de.rub.nds.tlsscanner.serverscanner.probe.bleichenbacher.vector.Pkcs1VectorGenerator;
 import de.rub.nds.tlsscanner.serverscanner.report.ServerReport;
+import java.io.IOException;
 import java.security.PublicKey;
+import java.security.cert.CertificateParsingException;
 import java.security.interfaces.RSAPublicKey;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bouncycastle.crypto.tls.Certificate;
+import org.bouncycastle.jce.provider.X509CertificateObject;
 
 public class BleichenbacherSubtask extends EvaluationSubtask {
 
@@ -60,11 +73,11 @@ public class BleichenbacherSubtask extends EvaluationSubtask {
         if (cipherSuite != null && version != null) {
             if(evaluationConfig.getTargetManagement() == DockerTargetManagement.RESTART_CONTAINTER) {
                parentTask.restartContainer(); 
-            } else if(evaluationConfig.getTargetManagement() == DockerTargetManagement.RESTART_CONTAINTER) {
+            } else if(evaluationConfig.getTargetManagement() == DockerTargetManagement.RESTART_SERVER) {
                 parentTask.restartServer();
             }
             
-            publicKey = CertificateFetcher.fetchServerPublicKey(getBaseConfig(version, cipherSuite));
+            publicKey = fetchServerPublicKey(getBaseConfig(version, cipherSuite));
             if (publicKey == null) {
                 LOGGER.error("Failed to fetch any PublicKey for {}", getTargetName());
                 return;
@@ -131,6 +144,49 @@ public class BleichenbacherSubtask extends EvaluationSubtask {
         }
         //ensure that we got SH + CERT + SKE + SHD
         return ((ReceiveTillAction)executedTrace.getFirstReceivingAction()).executedAsPlanned();
+    }
+    
+    
+    private PublicKey fetchServerPublicKey(Config config) {
+        X509CertificateObject cert;
+        try {
+            Certificate fetchedServerCertificate = fetchServerCertificate(config);
+            if (fetchedServerCertificate != null && fetchedServerCertificate.getLength() > 0) {
+                cert = new X509CertificateObject(fetchedServerCertificate.getCertificateAt(0));
+                return cert.getPublicKey();
+            }
+        } catch (CertificateParsingException ex) {
+            throw new WorkflowExecutionException("Could not get public key from server certificate", ex);
+        }
+        return null;
+    }
+    
+    private Certificate fetchServerCertificate(Config config) {
+        WorkflowConfigurationFactory factory = new WorkflowConfigurationFactory(config);
+        WorkflowTrace trace = factory.createTlsEntryWorkflowTrace(config.getDefaultClientConnection());
+        trace.addTlsAction(new SendAction(new ClientHelloMessage(config)));
+        if (config.getHighestProtocolVersion().isDTLS()) {
+            trace.addTlsAction(new ReceiveAction(new HelloVerifyRequestMessage(config)));
+            trace.addTlsAction(new SendAction(new ClientHelloMessage(config)));
+        }
+        trace.addTlsAction(new ReceiveTillAction(new CertificateMessage(config)));
+        State state = new State(config, trace);
+
+        
+        WorkflowExecutor workflowExecutor =
+            WorkflowExecutorFactory.createWorkflowExecutor(config.getWorkflowExecutorType(), state);
+        prepareExecutor(workflowExecutor);
+        try {
+            workflowExecutor.executeWorkflow();
+
+            if (!state.getTlsContext().getTransportHandler().isClosed()) {
+                state.getTlsContext().getTransportHandler().closeConnection();
+            }
+        } catch (IOException | WorkflowExecutionException e) {
+            LOGGER.warn("Could not fetch ServerCertificate");
+            LOGGER.debug(e);
+        }
+        return state.getTlsContext().getServerCertificate();
     }
 
 }
